f607ab8d721be1dc067b2c2f551ab199e92f0e77
Jenan-120 <noonaalhindi97@gmail.com>
2020-06-27 20:49:25 +0300

    شكرررررا شكررررا

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,29 +1,34 @@
 // السؤال الأول
 // 1
-
+const name="jenan";
 // 2
-
+let age="17";
 // 3 and 4
-
+console.log("انااسمي"+name
++age);
 // 5 and 6
-
+console.log (age*5);
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
-
+let hobbies=["reading","draw","writing"];
 // 2
-
+hobbies.push(age);
 // 3
-
+hobbies.pop();
 // 4
+let web={
+    track:"web",
+    languge:"js",
+    ta:["modhi","rahaf","nancy","hussain","menawer"],
 
+};
 // 5
-
 // 6
-
+console.log(web);
 // 7
-
+console.log(web.ta[0])
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
